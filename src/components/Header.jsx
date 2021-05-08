c4524bbc17e8317b7a7e479e88ab15b904865037
Dima <p>
2021-05-08 18:03:50 +0300

    Social netwok 1 commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://www.kuda.agency/wp-content/uploads/2017/01/cropped-favicon.png" alt="png"/>
+            <div>
+                <a href="#s"> Home -</a>
+                <a href="#s"> News -</a>
+                <a href="#s"> Feed -</a>
+                <a href="#s"> Message</a>
+            </div>
+        </header>
+    )
+
+}
+
+export default Header;
