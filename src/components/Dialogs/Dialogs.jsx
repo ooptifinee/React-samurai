3f460863d370421d77ecbb9b229e8d0c389ded1b
ooptifinee <pozitiw4ik01@gmail.com>
2021-05-30 21:35:33 +0300

    Social-network-6.5

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import Style from './Dialogs.module.css'
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
         <BrowserRouter>
-            <div>
-                dialogs
+            <div className={Style.chats}>
+                <div className={Style.Dialogs}>
+                    <div><NavLink to="/dialogs/1"> user 1</NavLink></div>
+                    <div><NavLink to="/dialogs/2"> user 2</NavLink></div>
+                    <div><NavLink to="/dialogs/3"> user 3</NavLink></div>
+                    <div><NavLink to="/dialogs/4"> user 4</NavLink></div>
+                </div>
+                <div className={Style.Messages}>
+                    <div>hi</div>
+                    <div>how are your</div>
+                    <div>Goodbye</div>
+                </div>
             </div>
         </BrowserRouter>
     )
