cabe075a4860bb967a4fba40816d9b6fcdaf7acd
Dima <->
2021-11-22 19:42:37 +0200

    Social-network -- created "Common/Preloader/Preloader.jsx" folder, added Preloader image (assets\image\Spiner-1s-200px.svg), added logic "true/false" for the Preloader.jsx

---
+++ src/components/Common/Preloader/Preloader.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import prelaoder from "../../../assets/images/Spinner-1s-200px.svg";
+
+let Preloader = (props) => {
+    return <img src={prelaoder}/>
+}
+
+export default Preloader;
